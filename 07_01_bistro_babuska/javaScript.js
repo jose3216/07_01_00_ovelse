3e760a45d94cb3b8ed4eee44b6e25d1bfa421d02
Jose Hartkopp <josehartkopp@Joses-MacBook-Pro.local>
2021-02-08 13:57:09 +0100

    content sat ind i template. med loop

--- 07_01_bistro_babuska/javaScript.js
+++ 07_01_bistro_babuska/javaScript.js
@@ -1,4 +1,4 @@
-const medieurl = "https://babushka-dd8a.restdb.io/media";
+const medieurl = "https://babushka-dd8a.restdb.io/media/";
 const myHeaders = {
 
 	"x-apikey": "600ec2fb1346a1524ff12de4"
@@ -18,6 +18,18 @@ async function loadJSON() {
 	visRetter();
 }
 
+const section = document.querySelector("section");
+const template = document.querySelector("template").content;
+
 function visRetter() {
 	console.log(retter);
+	retter.forEach(ret => {
+		const klon = template.cloneNode(true);
+		klon.querySelector(".pic").src = medieurl + ret.billede;
+		klon.querySelector(".navn").textContent = ret.navn;
+		klon.querySelector(".pris").textContent = ret.pris;
+		klon.querySelector(".k_tekst").textContent = ret.kortbeskrivelse;
+		section.appendChild(klon);
+	})
+
 }
