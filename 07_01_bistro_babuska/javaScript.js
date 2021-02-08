0c32354a04c284b6d0b5f84010a3dc31a981bafe
Jose Hartkopp <josehartkopp@Joses-MacBook-Pro.local>
2021-02-08 14:50:10 +0100

    Filtrer efter kategorier

--- 07_01_bistro_babuska/javaScript.js
+++ 07_01_bistro_babuska/javaScript.js
@@ -1,12 +1,26 @@
+const over = document.querySelector("h1");
 const medieurl = "https://babushka-dd8a.restdb.io/media/";
 const myHeaders = {
 
 	"x-apikey": "600ec2fb1346a1524ff12de4"
 }
 document.addEventListener("DOMContentLoaded", start)
+let retter;
+let filter = "alle";
 
 function start() {
+	const filterknapper = document.querySelectorAll("nav button");
+	filterknapper.forEach(knap => knap.addEventListener("click", filtreRetter))
 	loadJSON();
+}
+
+function filtreRetter() {
+	filter = this.dataset.kategori;
+	over.textContent = this.textContent;
+	document.querySelector(".valgt").classList.remove("valgt");
+	this.classList.add("valgt");
+
+	visRetter();
 }
 
 async function loadJSON() {
@@ -18,18 +32,24 @@ async function loadJSON() {
 	visRetter();
 }
 
-const section = document.querySelector("section");
-const template = document.querySelector("template").content;
+
 
 function visRetter() {
-	console.log(retter);
+	const section = document.querySelector("section");
+	const template = document.querySelector("template").content;
+	section.textContent = "";
+
 	retter.forEach(ret => {
-		const klon = template.cloneNode(true);
-		klon.querySelector(".pic").src = medieurl + ret.billede;
-		klon.querySelector(".navn").textContent = ret.navn;
-		klon.querySelector(".pris").textContent = ret.pris;
-		klon.querySelector(".k_tekst").textContent = ret.kortbeskrivelse;
-		section.appendChild(klon);
+		console.log("Menu", ret.kategori);
+
+		if (filter == ret.kategori || filter == "alle") {
+			const klon = template.cloneNode(true);
+			klon.querySelector(".pic").src = medieurl + ret.billede;
+			klon.querySelector(".navn").textContent = ret.navn;
+			klon.querySelector(".pris").textContent = ret.pris;
+			klon.querySelector(".k_tekst").textContent = ret.kortbeskrivelse;
+			section.appendChild(klon);
+		}
 	})
 
 }
